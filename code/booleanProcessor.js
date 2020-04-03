e4c9f54c5a499b5e693cbb789b02ef309b345ac2
Nicolas Salom <nsalom@icloud.com>
2020-04-03 13:46:09 -0400

    init2

--- code/booleanProcessor.js
+++ code/booleanProcessor.js
@@ -1,5 +1,13 @@
 module.exports = {
     getOpposite: function(a) {
         return !a;
+    },
+
+    isTrue: function(a){
+        return a === true;
+    },
+
+    isFalse: function(a){
+        return a === false;
     }
 };
--- spec/booleanProcessor.spec.js
+++ spec/booleanProcessor.spec.js
@@ -24,3 +24,29 @@ describe('Tests the getOpposite function', () => {
         expect(result).toBe(true);
     })
 });
+
+describe('Tests the isTrue function', () => {
+    it('Gets in true, returns true', () => {
+        let bool = true;
+        const result = bp.isTrue(bool);
+        expect(result).toBe(true);
+    });
+    it('Gets in false, returns false', () => {
+        let bool = false;
+        const result = bp.isTrue(bool);
+        expect(result).toBe(false);
+    });
+});
+
+describe('Tests the isFalse function', () => {
+    it('Gets in true, returns false', () => {
+        let bool = true;
+        const result = bp.isFalse(bool);
+        expect(result).toBe(false);
+    });
+    it('Gets in false, returns true', () => {
+        let bool = false;
+        const result = bp.isFalse(bool);
+        expect(result).toBe(true);
+    });
+});
